0400e954f41cad71e786d5804e21e70dc1f2a42f
Luciano Garcia <luciano.garcia@ut.ee>
2017-03-17 12:18:47 +0200

    initial commit

---
+++ src/main/java/com/buildit/procurement/rest/controller/ProcurementRestController.java
@@ -0,0 +1,35 @@
+package com.buildit.procurement.rest.controller;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by lgarcia on 3/16/2017.
+ */
+@RestController
+@RequestMapping("/api/procurements")
+public class ProcurementRestController {
+
+    @GetMapping("/plants")
+    public List<?> findAvailablePlants (
+            @RequestParam(name = "name", required = false) Optional<String> plantName,
+            @RequestParam(name = "startDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Optional<LocalDate> startDate,
+            @RequestParam(name = "endDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Optional<LocalDate> endDate) {
+
+//        if (plantName.isPresent() && startDate.isPresent() && endDate.isPresent()) {
+//            if (endDate.get().isBefore(startDate.get()))
+//                throw new IllegalArgumentException("Something wrong with the requested period ('endDate' happens before 'startDate')");
+            return null;
+//        } else
+//            throw new IllegalArgumentException(
+//                    String.format("Wrong number of parameters: Name='%s', Start date='%s', End date='%s'",
+//                            plantName.get(), startDate.get(), endDate.get()));
+    }
+}
