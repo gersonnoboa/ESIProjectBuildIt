d0bb8535c67a66bb5953d388ab94b5892c8529d7
Geraldine Granada <gkgranada@gmail.com>
2017-03-21 15:55:15 +0200

    Add practical work

---
+++ src/main/java/com/buildit/rental/application/dto/PlantInventoryEntryDTO.java
@@ -0,0 +1,14 @@
+package com.buildit.rental.application.dto;
+
+import com.buildit.common.rest.ResourceSupport;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class PlantInventoryEntryDTO extends ResourceSupport {
+    String _id;
+    String name;
+    String description;
+    BigDecimal price;
+}
---
+++ src/main/java/com/buildit/rental/application/service/RentalService.java
@@ -0,0 +1,26 @@
+package com.buildit.rental.application.service;
+
+import com.buildit.rental.application.dto.PlantInventoryEntryDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by gkgranada on 17/03/2017.
+ */
+@Service
+public class RentalService {
+    @Autowired
+    RestTemplate restTemplate;
+
+    public List<PlantInventoryEntryDTO> findAvailablePlants(String plantName, LocalDate startDate, LocalDate endDate) {
+        PlantInventoryEntryDTO[] plants = restTemplate.getForObject(
+                "http://localhost:8090/api/inventory/plants?name={name}&startDate={start}&endDate={end}",
+                PlantInventoryEntryDTO[].class, plantName, startDate, endDate);
+        return Arrays.asList(plants);
+    }
+}
