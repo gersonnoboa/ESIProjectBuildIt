d83b0ecc21dd3b4e73a80045a8f6507870b805e0
Geraldine Granada <gkgranada@gmail.com>
2017-05-26 13:16:28 +0300

    Add login screen and thymeleaf security dependency

---
+++ src/main/java/com/buildit/common/security/IndexController.java
@@ -0,0 +1,31 @@
+package com.buildit.common.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by gkgranada on 26/05/2017.
+ */
+@Controller
+public class IndexController implements org.springframework.boot.autoconfigure.web.ErrorController{
+
+
+    @GetMapping("/login")
+    public String getLoginForm(Model model) {
+        return "dashboard/login";
+    }
+
+    // Login form with error
+    @RequestMapping("/error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "/dashboard/error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
--- src/main/java/com/buildit/common/security/SecurityConfiguration.java
+++ src/main/java/com/buildit/common/security/SecurityConfiguration.java
@@ -9,10 +9,17 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -50,6 +57,25 @@ public class SecurityConfiguration {
                     .authenticationEntryPoint((req,res,exc) ->
                         res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "You don't have anything to see here"));
             http.headers().frameOptions().sameOrigin();
+            //Thymeleaf dashboard
+            http
+                    .formLogin()
+                    .loginPage("/dashboard/login.html")
+                    .failureUrl("/dashboard/error.html")
+                    .and()
+                    .logout()
+                    .logoutSuccessUrl("/dashboard/login.html");
+            RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+            http
+                    .formLogin()
+                    .loginPage("/login")
+                    .successHandler(new AuthenticationSuccessHandler() {
+                        @Override
+                        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                            Authentication cauthentication) throws IOException, ServletException {
+                            redirectStrategy.sendRedirect(request, response, "/dashboard/plants/form");
+                        }
+                    });
         }
     }
 }
